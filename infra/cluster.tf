6c08af132ac0498dcb560a0e87dd5162178970a1
Tiago Simionato <57016670+TiagoSimionato@users.noreply.github.com>
2026-09-14 19:56:31 -0300

    feat: traefik (#5)

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -8,9 +8,27 @@ resource "kind_cluster" "main" {
 
     node {
       role = "control-plane"
+      kubeadm_config_patches = [
+        <<-EOT
+        kind: InitConfiguration
+        nodeRegistration:
+          kubeletExtraArgs:
+            node-labels: "ingress-ready=true"
+        EOT
+      ]
       extra_port_mappings {
         container_port = 30000
         host_port      = 3000
+        protocol       = "TCP"
+      }
+      extra_port_mappings {
+        container_port = 30080
+        host_port      = 8000
+        protocol       = "TCP"
+      }
+      extra_port_mappings {
+        container_port = 30900
+        host_port      = 9000
         protocol       = "TCP"
       }
     }
