508d3b7f615774927e3eec76745159c65506645e
Tiago Simionato <57016670+TiagoSimionato@users.noreply.github.com>
2026-09-14 17:17:32 -0300

    fea: infra (#1)
    * feat: infra
    
    * feat: hpa
    
    * fix: tfstate path

---
+++ infra/vars.tf
@@ -0,0 +1,7 @@
+variable "cluster_name" {
+  default = "soat-cluster"
+}
+
+variable "namespace" {
+  default = "soat-grupo76"
+}
---
+++ infra/versions.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "local" {
+    path = "/opt/terraform/k8/terraform.tfstate"
+  }
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "~> 0.6"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+  }
+}
+
+provider "kind" {}
+
+provider "kubernetes" {
+  host                   = kind_cluster.main.endpoint
+  client_certificate     = kind_cluster.main.client_certificate
+  client_key             = kind_cluster.main.client_key
+  cluster_ca_certificate = kind_cluster.main.cluster_ca_certificate
+}
+
+locals {
+  metrics_server_raw = split("---", file("${path.module}/../k8s/metrics.yml"))
+
+  metrics_server_docs = {
+    for doc in local.metrics_server_raw :
+    "${yamldecode(doc).kind}-${yamldecode(doc).metadata.name}" => yamldecode(doc)
+    if trimspace(doc) != ""
+  }
+}
